49bca16580f07e1d90e388f0bd2b98770b6f1ada
B Spinks <beauspinks@gmail.com>
2020-03-23 17:41:34 +0000

    more minor changes

--- PythonPractice/First.py
+++ PythonPractice/First.py
@@ -1,3 +1,15 @@
+# -----------------------------------------------------------
+# Hi there, my name is Beaumont Spinks and this is my practice file for Python
+# Big thanks to "Programming with Mosh" on youtube
+#
+# The purpose of this file is to be used as a reminder for myself for Python
+# To help me differentiate the subtle language differences between programming languages
+#
+# (C) 2020 Beaumont Spinks, London, England
+# email beauspinks@gmail.com
+# -----------------------------------------------------------
+
+
 print('Beaumont Spinks') # Basic print function
 print('*' * 10) # This is known as an expression, its similar to a math equation
 price = 10 # A variable, set to an Integer
@@ -38,7 +50,7 @@ This will all be printed line by line.
 Kind Regards,
 
 Spinks
-'''
+''' # have to put a commit after the final ''' because all lines in between are treated as a single paragraph of code
 print(course)
 course = 'Python course for Beginners'
 print(course[0]) # Square brackets is used to print specific characters in a string, which start at 0
@@ -52,7 +64,57 @@ print(course[1:-1])
 first = 'John'
 last = 'Smith'
 print(first + ' [' + last + '] ' + ' is a coder') # default way to print with variables
-print(f'{first} [{last}] is a coder') # formatted string to print with varuables
+print(f'{first} [{last}] is a coder') # formatted string to print with variables
 print(len(course)) # len is short for length
-course.upper() # object oriented functions build into python
-course.lower() # also known as dot operators, or methods
+print(course.upper()) # object oriented functions build into python
+print(course.lower()) # also known as dot operators, or methods
+print(course.title()) # first letter of each word is capitalized
+print(course.find('P')) # method to find a character
+print(course.find('0')) # if no character is found will return -1
+print(course.find('Beginners'))
+print(course.replace('Beginners', 'Absolute Beginners')) # method to find and replace sequence of characters
+print(course.replace('P', 'J'))
+print('Python' in course) # in operator checks if characters are in a string, returns boolean
+print(10 + 3 - 6 * 2 / 4) # arithmetic operators
+print(5 / 3) # division, returns a float
+print(5 // 3) # division, returns an integer, always rounds down
+print(5 % 3) # returns the remainder of a division
+print(5 ** 3) # exponentiation or power operator, e.g. 5 to the power of 3
+x = 10
+x = x + 3 # incrementing operator
+x += 3 # enhances augmenter operator
+x -= 5
+print(10 + (3 - 6) / 7 * 11 ** 4) # BIDMAS is law in python
+x = 2.9
+print(round(x))
+print(abs(x)) # math methods, more with the math module
+
+
+'''
+if it's hot
+    It's a hot day
+    Drink plenty of water   
+otherwise if it's cold
+    It's a cold day
+    Wear warm clothes
+otherwise
+    It's a lovely day
+''' # when programming large sections of code it can help to preplan the layout
+
+is_hot = False
+
+if is_hot: # if statements to check and run code under conditions set, boolean check if 'True'
+    print("It's a hot day")
+print('Enjoy your day!') # Python is incredible indent reliant, this line of code is not included in the 'if' function
+
+is_cold = False
+
+if is_hot: # colon to open if statements along with else and elif statements
+    print("It's a hot day")
+    print('Drink plenty of water')
+elif is_cold: # elif is used after if where there are multiple conditions to be checked
+    print("It's a cold day")
+    print('Wear warm clothes')
+else:
+    print("It's a lovely day")
+print('Enjoy your day!')
